c32caaad6e104e73ca36412bc5ad6825b073ab07
henry-w-slough <henry.w.slough@gmail.com>
2026-04-15 09:47:26 -0400

    Added basic Movie model

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,9 +1,19 @@
 
+
+
+/// <summary>
+/// Instance of movie data-related information for configuration, referencing, and storage.
+/// </summary>
 public class Movie
 {
-    
 
-    public Movie () {
-        
+    string Id {get; set;}
+    string Name {get; set;}
+
+
+    public Movie (string name, string id) {
+        Name = name;
+        //Note: Even though Id is set through a parameter, Id should be generated in a Service nontheless
+        Id = id;
     }
 }
