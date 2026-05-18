9dfd6dafb272a11f8d59a09496642eb43a1850af
henry-w-slough <henry.w.slough@gmail.com>
2026-05-18 09:54:58 -0400

    Urls now align with backends

---
+++ Migrations/20260518131953_AddedMovies.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace netmovies_metadata_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedMovies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateAdded = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StorageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
--- obj/Debug/net10.0/netmovies-metadata-api.AssemblyInfo.cs
+++ obj/Debug/net10.0/netmovies-metadata-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("netmovies-metadata-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4764ff8aa7c585acb13cfac9961678ece9836d6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("netmovies-metadata-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("netmovies-metadata-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
